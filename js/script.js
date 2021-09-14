5e407aba421e534c0005eb8298c32d12ef4b58ca
takahashi21 <takahashi210517@gmail.com>
2021-09-14 12:37:23 +0900

    modal display ,CSS＞Sass

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 /* JavaScript */
 MicroModal.init({
-/*   onShow: modal => console.info(`${modal.id} is shown`), // [1]
-  onClose: modal => console.info(`${modal.id} is hidden`), // [2]
-  openTrigger: 'data-custom-open', // [3]
-  closeTrigger: 'data-custom-close', // [4]
-  openClass: 'is-open', // [5]
+  //onShow: modal => console.info(`${modal.id} is shown`), // [1]
+  //onClose: modal => console.info(`${modal.id} is hidden`), // [2]
+  //openTrigger: 'data-custom-open', // [3]
+  //closeTrigger: 'data-custom-close', // [4]
+  //openClass: 'is-open', // [5]
   disableScroll: true, // [6]
-  disableFocus: false, // [7]
-  awaitOpenAnimation: false, // [8]
-  awaitCloseAnimation: false, // [9]
-  debugMode: true // [10] */
+  //disableFocus: false, // [7]
+  //awaitOpenAnimation: false, // [8]
+  //awaitCloseAnimation: false, // [9]
+  //debugMode: true // [10] */
 });
 
 /* jQuery */
@@ -54,7 +54,7 @@ var pos04 = Math.round($('#contact').offset().top - 80);
   else if(scrl>=pos02 && scrl<pos03){
   $('#ls-2').addClass('current').siblings('li').removeClass('current');}
   //skill < scrl < contact
-  else if(scrl>=pos02 && scrl<pos03){
+  else if(scrl>=pos03 && scrl<pos04){
   $('#ls-3').addClass('current').siblings('li').removeClass('current');}
   //scrl => contact
   else{$('#ls-4').addClass('current').siblings('li').removeClass('current');}
