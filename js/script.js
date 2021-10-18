88e8fc1d485f4b1b816ab1ec44cfc0837b353798
takahashi21 <takahashi210517@gmail.com>
2021-10-18 14:32:39 +0900

    style.css ,index.html modified

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ $(scrlbtn).on('click',function(){
 
 const scrltgt = $(this).attr('href');
 
-$('html,body').animate({scrollTop:$(scrltgt).offset().top - 80},600);
+$('html,body').animate({scrollTop:$(scrltgt).offset().top},400,'swing');
 return false;
 
 });
@@ -59,28 +59,6 @@ $(toTop).fadeIn();
 }else{
 $(toTop).hide();
 }
-
-//ul current display
-const scrl = $(this).scrollTop();
-const pos01 = Math.round($('#about').offset().top - 160);
-const pos02 = Math.round($('#work').offset().top - 160);
-const pos03 = Math.round($('#skill').offset().top - 160);
-
-  // 0 < scrl < about
-  if(0<=scrl && scrl<pos01){
-  $('#ls-0').addClass('current').siblings('li').removeClass('current');}
-
-  // about < scrl < work
-  else if(scrl >= pos01 && scrl<pos02 ){
-  $('#ls-1').addClass('current').siblings('li').removeClass('current');}
-
-  // work < scrl < skill
-  else if(scrl>=pos02 && scrl<pos03 ){
-  $('#ls-2').addClass('current').siblings('li').removeClass('current');}
-
-  // skill < scrl
-  else{
-  $('#ls-3').addClass('current').siblings('li').removeClass('current');}
 
 });
 
